ed8a9d291aab00973d02eec8abde54690cceb376
Ubuntu <ubuntu@ip-172-31-57-71.ec2.internal>
2021-01-23 08:49:37 +0000

    Add Login Logout setting

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root 'staticpages#home'
   
   resources :users
-
+  
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 end
 
